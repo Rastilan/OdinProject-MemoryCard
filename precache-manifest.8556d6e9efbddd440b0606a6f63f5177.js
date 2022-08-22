321cfd93ccb3af678b9feaed9bbafc92337d4caf
Rastilan <christoperbhall@gmail.com>
2022-08-22 00:24:54 -0400

    Updates

---
+++ precache-manifest.8556d6e9efbddd440b0606a6f63f5177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1b477f724e090873457",
+    "url": "/OdinProject-MemoryCard/static/css/main.e7227168.chunk.css"
+  },
+  {
+    "revision": "c1b477f724e090873457",
+    "url": "/OdinProject-MemoryCard/static/js/main.c1b477f7.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/OdinProject-MemoryCard/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/OdinProject-MemoryCard/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "eed22f2382f1dfcd2d90",
+    "url": "/OdinProject-MemoryCard/static/js/runtime~main.eed22f23.js"
+  },
+  {
+    "revision": "4b4be53f1aef359893ae583616bb5202",
+    "url": "/OdinProject-MemoryCard/index.html"
+  }
+];
